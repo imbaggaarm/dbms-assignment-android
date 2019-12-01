8a4d929091c3b618391fd1400e6185dec79ea1f2
imbaggaarm <imbaggaarm@gmail.com>
2019-12-01 20:21:56 +0700

    create project

---
+++ app/src/main/java/com/imbaggaarm/taiduong/dbsassignment/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.imbaggaarm.taiduong.dbsassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
+
